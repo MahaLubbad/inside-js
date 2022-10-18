5e91f4efea4e09d3a502ec84a76a8edb2e4c7e55
MahaLubbad <97481258+MahaLubbad@users.noreply.github.com>
2022-10-18 19:02:53 +0200

    Solution-2 Added

--- Exerscises(Ufuk)/Question-2.js
+++ Exerscises(Ufuk)/Question-2.js
@@ -4,7 +4,7 @@ Write a function that checks the occurrence of "a" and "b" to see
 if a string has the same amount of 'a's and 'b's. The method must return
  a boolean and be case insensitive. The string can contain any char.
 */
-
+/*
 function abTest (str) {
 // logic here
 let countA = 0;
@@ -17,17 +17,28 @@ if(str[i]==='b'||str[i]==='B'){
   countB +=1 ;
 }
 }
-let abCheck = false;
-if (countA === countB){
-  abCheck = true;
+
+return countA === countB;
 }
-return abCheck;
+*/
+function abTest (str) {
+
+ const arr = str.split('');
+  const charA = arr.filter((char) => char==='a'|| char === 'A' );
+ const  aLength = charA.join().length ;
+
+  const charB = arr.filter((char) => char ==='b' || char === 'B' );
+  const  bLength = charB.join().length ;
+  
+return aLength === bLength ;
 }
+
 console.log(abTest('aabb'));
 console.log(abTest('baabb'));
 console.log(abTest('aabBm'));
 console.log(abTest('zpzpzpp'));
 console.log(abTest('zzaa'));
+
 /*
 abTest("aabb") => should return true
 abTest("baabb") => should return false
